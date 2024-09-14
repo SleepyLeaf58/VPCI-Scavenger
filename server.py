1ec3d144735524a9f2202c1d03362a346d328e1a
Savir Singh (Saroha) <savirsaroha@gmail.com>
2024-09-14 16:21:52 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import *
 import random
 
 app = Flask(__name__)
@@ -9,31 +9,49 @@ players = {}  # store players and their progress
 # Route for organizers to start a hunt
 @app.route("/start-hunt", methods=['POST'])
 def start_hunt():
-    data = request.json
+    hunt_name = request.form['roomName']
+    organizer = request.form['org']
+    riddle1 = request.form['riddle1']
+    riddle2 = request.form['riddle2']
+    riddle3 = request.form['riddle3']
+    room1 = request.form['room1']
+    room2 = request.form['room2']
+    room3 = request.form['room3']
+    code1 = request.form['code1']
+    code2 = request.form['code2']
+    code3 = request.form['code3']
     hunt_id = random.randint(1000, 9999)
     hunts[hunt_id] = {
-        'hunt_name': data['hunt_name'],
-        'organizer': data['organizer'],
-        'objects': data['objects'],  # list of items with room and riddle
+        'hunt_name': hunt_name,
+        'organizer': organizer,
+        'objects': [{"riddle":riddle1, "room":room1, "code": code1}, {"riddle":riddle2, "room":room2, "code": code2}, {"riddle":riddle3, "room":room3, "code": code3}],  # list of items with room, riddle,code
         'players': []
     }
-    return jsonify({"message": f"Hunt {hunt_id} created!", "hunt_id": hunt_id})
+    return f"Hunt with name {hunt_name} created! The id for this hunt is {hunt_id}"
 
 # Route for players to join a hunt
-@app.route("/join-hunt", methods=['POST'])
+@app.route("/join-hunt", methods=["POST"])
 def join_hunt():
-    data = request.json
-    hunt_id = data['hunt_id']
+    hunt_id = int(request.form["idd"])
+    name = request.form["name"]
     if hunt_id in hunts:
         player_id = random.randint(1000, 9999)
-        player = {'name': data['name'], 'current_object': 0, 'finished': False}
+        player = {'name': name, 'current_object': 0, 'finished': False}
         hunts[hunt_id]['players'].append(player_id)
         players[player_id] = player
         first_hint = hunts[hunt_id]['objects'][0]['riddle']
         first_room = hunts[hunt_id]['objects'][0]['room']
-        return jsonify({"message": "Joined hunt!", "player_id": player_id, "first_hint": first_hint, "first_room": first_room})
+        return render_template("save-local.html", player_id=player_id)
     else:
-        return jsonify({"error": "Hunt not found"}), 404
+        return jsonify({"error": "This hunt is over."}), 404
+
+@app.route("/current-riddle", methods=["POST"])
+def current_riddle():
+    player_id = request.form["player_id"]
+    player = players[player_id]
+    next_hint = hunts[hunt_id]['objects'][player['current_object']]['riddle']
+    next_room = hunts[hunt_id]['objects'][player['current_object']]['room']
+    return render_template("player-dashboard.html", riddle=next_hint, room=next_room, obj=player['current_object'])
 
 # Route for players to submit found item codes
 @app.route("/submit-item", methods=['POST'])
